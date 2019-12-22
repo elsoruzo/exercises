c4bcee59d0f600884155c650e7bbaaa949cfc124
Yakov Voitsekhovsky <Yakov.Voitsekhovsky@emergn.com>
2019-12-22 22:09:45 +0300

    minor refactoring. added ChainOfResponsibility pattern

---
+++ dessign_patterns/dessign_patterns_exercises/design_patterns/BehavioralPatterns/ChainOfResponsibilityModel/IHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace design_patterns_exercises.BehavioralPatterns.ChainOfResponsibilityModel
+{
+    // Интерфейс Обработчика объявляет метод построения цепочки обработчиков. Он
+    // также объявляет метод для выполнения запроса.
+    public interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+
+        object Handle(object request);
+    }
+}
--- dessign_patterns/dessign_patterns_exercises/design_patterns/Program.cs
+++ dessign_patterns/dessign_patterns_exercises/design_patterns/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using design_patterns_exercises.FactoryModel;
 using design_patterns_exercises.AdapterModel;
+using design_patterns_exercises.BehavioralPatterns.ChainOfResponsibilityModel;
 
 namespace design_patterns_exercises
 {
@@ -8,6 +9,22 @@ namespace design_patterns_exercises
     {
         static void Main(string[] args)
         {
+            // Другая часть клиентского кода создает саму цепочку.
+            var monkey = new MonkeyHandler();
+            var squirrel = new SquirrelHandler();
+            var dog = new DogHandler();
+
+            monkey.SetNext(squirrel).SetNext(dog);
+
+            // Клиент должен иметь возможность отправлять запрос любому
+            // обработчику, а не только первому в цепочке.
+            Console.WriteLine("Chain: Monkey > Squirrel > Dog\n");
+            ChainOfResponsibilityClient.ClientCode(monkey);
+            Console.WriteLine();
+
+            Console.WriteLine("Subchain: Squirrel > Dog\n");
+            ChainOfResponsibilityClient.ClientCode(squirrel);
+
             Console.BackgroundColor = ConsoleColor.Yellow;
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("START RUNNING FACTORY.");
