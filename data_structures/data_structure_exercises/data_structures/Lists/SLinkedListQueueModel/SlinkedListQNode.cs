562e05b2df9884b4619dfb30bbb80bc43657357d
Yakov Voitsekhovsky <Yakov.Voitsekhovsky@emergn.com>
2019-12-22 19:52:50 +0300

    Added queue realization based on linked list

---
+++ data_structures/data_structure_exercises/data_structures/Lists/SLinkedListQueueModel/SlinkedListQNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace data_structures.Lists.SLinkedListQueueModel
+{
+    public class SlinkedListQNode
+    {
+        public int key;
+        public SlinkedListQNode next;
+
+        // constructor to create 
+        // a new linked list node 
+        public SlinkedListQNode(int key)
+        {
+            this.key = key;
+            this.next = null;
+        }
+    }
+}
