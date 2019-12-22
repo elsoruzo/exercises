029e5a9f9edb1c4ff9e42a19ffdbbf0f1c645e3b
Yakov Voitsekhovsky <Yakov.Voitsekhovsky@emergn.com>
2019-12-22 18:58:40 +0300

    added BinarySearch,  Recursive  and Iterative, made it nullable

---
+++ algorithms/AlgorithmsExercises/AlgorithmsExercises/SearchingAlgorithms/BinarySearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsExercises.SearchingAlgorithms
+{
+    class BinarySearch
+    {
+        public static int? BinarySearchIterative(int[] inputArray, int key)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (key == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (key < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return null;
+        }
+
+        public static int? BinarySearchRecursive(int[] inputArray, int key, int min, int max)
+        {
+            if (min > max)
+            {
+                return null;
+            }
+            else
+            {
+                int mid = (min + max) / 2;
+                if (key == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (key < inputArray[mid])
+                {
+                    return BinarySearchRecursive(inputArray, key, min, mid - 1);
+                }
+                else
+                {
+                    return BinarySearchRecursive(inputArray, key, mid + 1, max);
+                }
+            }
+        }
+    }
+}
