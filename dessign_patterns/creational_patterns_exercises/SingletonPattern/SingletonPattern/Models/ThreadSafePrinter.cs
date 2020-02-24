20acbf40f7e7773d2d4e2e2c699e8d9850ce6538
Yakov Voitsekhovsky <Yakov.Voitsekhovsky@emergn.com>
2020-02-25 01:21:20 +0300

    implemented SingletonPattern on ThreadSafePrinter domain Iteration-1

---
+++ dessign_patterns/creational_patterns_exercises/SingletonPattern/SingletonPattern/Models/ThreadSafePrinter.cs
@@ -0,0 +1,49 @@
+﻿namespace TreadSafePrinter.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+
+    public sealed class ThreadSafePrinter {  
+        // TODO VOLATILE ?
+    private static volatile ThreadSafePrinter _threadSafePrinterInstance;
+
+    private Queue<string> _queue = new Queue<string>();
+    private static string _instanceName;
+    private static object _syncRoot = new object();
+        // TODO SemaphoreSlim ?
+        static SemaphoreSlim _sem = new SemaphoreSlim(1);
+        private ThreadSafePrinter() { }
+        public static ThreadSafePrinter GetPrinterInstance(string instanceName)
+        {
+            if (_threadSafePrinterInstance == null)
+            {
+                // TODO lock ?
+                lock (_syncRoot)
+                {
+                //_sem.Wait();
+
+                if (_threadSafePrinterInstance == null)
+                    {
+                        _instanceName = instanceName;
+                        Console.WriteLine("{0} printer object created", instanceName);
+                        _threadSafePrinterInstance = new ThreadSafePrinter();
+                    }
+                }
+                //_sem.Release();
+            }
+            return _threadSafePrinterInstance;
+        }
+
+        internal void AddDocument(string documentName)
+        {
+            _queue.Enqueue(documentName);
+        }
+
+        internal void PrintDocument()
+        {
+            Console.WriteLine("Printing the content of the document: " +
+                "\n {0}", _queue.Dequeue());
+        }
+    }  
+}
---
+++ dessign_patterns/creational_patterns_exercises/SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace SingletonPattern
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using TreadSafePrinter.Models;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitializeThreadSafePrinterObject();
+        }
+
+        static void InitializeThreadSafePrinterObject()
+        {
+            ThreadSafePrinter firstPrinterObject = null;
+            ThreadSafePrinter secondPrinterObject = null;
+            ThreadSafePrinter thirdPrinterObject = null;
+
+           // Task vs Thrad ??
+
+            Task task1 = Task.Factory.StartNew(() =>
+            {
+                firstPrinterObject = GetThreadSafePrinterInstance("First", "First-Document");
+            });
+            Task task2 = Task.Factory.StartNew(() =>
+            {
+                secondPrinterObject = GetThreadSafePrinterInstance("Second", "Second-Document");
+            });
+            Task task3 = Task.Factory.StartNew(() =>
+            {
+                thirdPrinterObject = GetThreadSafePrinterInstance("Third", "Third-Document");
+            });
+            Task.WaitAll(task1, task2, task3);
+
+            Console.WriteLine("All threads complete");
+            Console.WriteLine("Are these First Printer Object And Second Printer Object Same - {0} ", firstPrinterObject.Equals(secondPrinterObject) ? "Yes" : "No");
+            Console.WriteLine("Are these First Printer Object And Third Printer Object Same - {0} ", firstPrinterObject.Equals(thirdPrinterObject) ? "Yes" : "No");
+            Console.WriteLine("Are these Second Printer Object And Third Printer Object Same - {0} ", secondPrinterObject.Equals(thirdPrinterObject) ? "Yes" : "No");
+
+            Thread process1 = new Thread(() =>
+            {
+                TestSingleton("process1");
+            });
+
+            Thread process2 = new Thread(() =>
+            {
+                TestSingleton("process2");
+            });
+
+            Thread process3 = new Thread(() =>
+            {
+                TestSingleton("process3");
+            });
+
+            process1.Start();
+            process1.Join();
+
+            process2.Start();
+            process3.Start();
+            process3.Join();
+            process2.Join();
+
+            Console.ReadLine();
+        }
+        private static ThreadSafePrinter GetThreadSafePrinterInstance(string instanceName, string documentName)
+        {
+            var printerObject = ThreadSafePrinter.GetPrinterInstance(instanceName);
+            printerObject.AddDocument(documentName);
+            printerObject.PrintDocument();
+            return printerObject;
+        }
+        public static void TestSingleton(string value)
+        {
+            ThreadSafePrinter singleton = ThreadSafePrinter.GetPrinterInstance(value);
+            singleton.AddDocument(value);
+            singleton.PrintDocument();
+        }
+    }
+}
